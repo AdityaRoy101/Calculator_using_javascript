e7563c81bbd090660e6cbd23f4555c0424c711b7
Aditya Roy <86155138+AdityaRoy101@users.noreply.github.com>
2023-06-09 15:54:52 +0530

    Main_javaScript_file

---
+++ calculatorJavascript.js
@@ -0,0 +1,17 @@
+///Code
+//---------
+var a = prompt("Enter the first number");
+var b = prompt("Enter the second number");
+var c = prompt("Enter the operation you want to perform");
+var a1 = Number(a);
+var a2 = Number(b);
+if(c === "+")
+alert("The sum of the number are : " + (a1 + a2));
+else if(c === "*")
+alert("The multiplication of the numbers are : " + (a1 * a2));
+else if(c === "-")
+alert("The substraction of the numbers are : " + (a1 - a2));
+else if(c === "/")
+alert("The division of the two numbers are : " + (a1 / a2));
+else
+alert("Wrong Choice");
